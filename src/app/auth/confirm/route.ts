117db9eac5e74f32c0576aa8688988e6954fb9c5
Alif Doloh <forza.dll350@gmail.com>
2026-03-06 15:23:04 +0700

    feat: add auth confirm route

---
+++ src/app/auth/confirm/route.ts
@@ -0,0 +1,26 @@
+﻿import { createClient } from '@/lib/supabase/server'
+import { NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams, origin } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type')
+
+  if (token_hash && type) {
+    const supabase = await createClient()
+    const { error } = await supabase.auth.verifyOtp({
+      token_hash,
+      type: type as any,
+    })
+
+    if (!error) {
+      if (type === 'recovery') {
+        return NextResponse.redirect(${origin}/auth/update-password)
+      }
+      return NextResponse.redirect(${origin})
+    }
+  }
+
+  return NextResponse.redirect(${origin}/?error=invalid_link)
+}
